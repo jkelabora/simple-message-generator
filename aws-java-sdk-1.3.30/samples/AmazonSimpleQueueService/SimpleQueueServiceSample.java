7ed1631f730a853e735613841f0c76b4b0907ba9
Julian Kelabora <jkelabora@dius.com.au>
2013-03-04 18:24:05 +1100

    basic java/ant mechanism to post a message to an amazon sqs queue

---
+++ aws-java-sdk-1.3.30/samples/AmazonSimpleQueueService/SimpleQueueServiceSample.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2010-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.DeleteQueueRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
+public class SimpleQueueServiceSample {
+
+    public static void main(String[] args) throws Exception {
+
+        AmazonSQS sqs = new AmazonSQSClient(new ClasspathPropertiesFileCredentialsProvider());
+        sqs.setEndpoint("sqs." + args[0] + ".amazonaws.com");
+
+        try {
+            String myQueueUrl = "";
+
+            for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+                if (queueUrl.endsWith(args[1])) {
+                    myQueueUrl = queueUrl;
+                }
+            }
+
+            if (myQueueUrl == "") throw new RuntimeException("Queue " + args[1] + " not found..") ;
+
+            System.out.println("Sending '" + args[2] + "' to " + myQueueUrl + "\n");
+            sqs.sendMessage(new SendMessageRequest(myQueueUrl, args[2]));
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it " +
+                    "to Amazon SQS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered " +
+                    "a serious internal problem while trying to communicate with SQS, such as not " +
+                    "being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+}
